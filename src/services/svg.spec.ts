c18a46d18ea7bc15e8f50ea0e552facd27e1b127
Loris Bognanni <loris@codemade.co.uk>
2021-11-07 17:15:34 +0000

    :tada: simple svg implementation

--- src/services/svg.spec.ts
+++ src/services/svg.spec.ts
@@ -1,4 +1,55 @@
-import { convertToSvg } from "./svg";
+import { ElementType, SvgItem } from "./parsertypes";
+import { convertToSvg, itemsToSvg } from "./svg";
+
+describe(`When converting items to svg string`, () => {
+    it(`Should create a simple svg item`, () => {
+        const items: SvgItem[] = [
+            {
+                x: 0,
+                y: 0,
+                text: "hello",
+                height: 0,
+                next: [],
+                type: ElementType.Root,
+                width: 0
+            }
+        ];
+        const result = itemsToSvg(items);
+        expect(result).toBe(`<g>
+<text x="0" y="0" font-size="10">hello</text>
+</g>`);
+    });
+    
+    it(`Should handle nested items`, () => {
+        const items: SvgItem[] = [
+            {
+                x: 0,
+                y: 0,
+                text: "hello",
+                height: 0,
+                next: [{
+                    x: 100,
+                    y: 0,
+                    text: "hi",
+                    height: 0,
+                    width: 0,
+                    next: [],
+                    type: ElementType.View
+                }],
+                type: ElementType.View,
+                width: 0
+            }
+        ];
+        const result = itemsToSvg(items);
+        expect(result).toBe(`<g>
+<text x="0" y="0" font-size="10">hello</text>
+</g>
+<g>
+<text x="100" y="0" font-size="10">hi</text>
+</g>`);
+    });
+});
+
 
 describe('When parsing a string to svg', () => {
 
@@ -10,6 +61,5 @@ describe('When parsing a string to svg', () => {
         
         expect(result).toBe(expected);
     });
-
 
 });
--- src/services/svg.ts
+++ src/services/svg.ts
@@ -1,6 +1,38 @@
+import { SvgItem } from "./parsertypes"
+
+function itemToSvg(item: SvgItem): string {
+    return `<text x="${item.x}" y="${item.y}" font-size="10">${item.text}</text>`
+}
+
+function groupToSvg(items: SvgItem[]): string[] {
+    const childItemGroups = [];
+    let lines = [];
+
+    if(items.length) {
+        lines.push("<g>");
+        for(const item of items){
+            lines.push(itemToSvg(item));
+            if(item.next) {
+                childItemGroups.push(item.next);
+            }
+        }
+        lines.push("</g>");
+
+        for(const childGroup of childItemGroups){
+            lines.push(... groupToSvg(childGroup))
+        }
+    }
+    return lines;
+}
+
+function itemsToSvg(items: SvgItem[]): string {
+    let arr = groupToSvg(items);
+    return arr.join("\n");
+}
+
 function convertToSvg(text:string) {
     return `<svg xmlns="http://www.w3.org/2000/svg">
 </svg>`
 }
 
-export { convertToSvg }
+export { convertToSvg, itemsToSvg }
