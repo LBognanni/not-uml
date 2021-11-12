06fa0b636973988263337dfd50c0f05e12befbb0
Loris Bognanni <loris@codemade.co.uk>
2021-11-12 20:08:38 +0000

    :memo: Add some basic instructions

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,12 @@ function App() {
       </div>
 
       <textarea value={text} onChange={(e) => convertSvg(e.target.value)}></textarea>
+      <div id="instructions">
+        <h4>How to</h4>
+        <p><b>Views</b> start with "<code>-&gt;</code>"</p>
+        <p><b>Actions</b> start with "<code>-</code>"</p>
+        <p>Indent actions under the parent view. Indent views under actions when taking the action will lead to the view.</p>
+      </div>
 
       <div className="chart-container">
         <div style={{width: svgWidth + "px"}} dangerouslySetInnerHTML={{ __html: svg }} />
